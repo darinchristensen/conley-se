d69aca4c81dca489f613b090d1685ae5efc128ca
Nicholas Potter <potterzot@gmail.com>
2018-11-01 15:06:15 -0700

    fix iterateObs() not being called with enough parameters.

--- code/conley.R
+++ code/conley.R
@@ -1,122 +1,150 @@
-#================================================================ 
+#================================================================
 ## Conley Standard Errors
 ## Date: August 21, 2017
 ## Vectorized Code using Rcpp
-#================================================================ 
+#================================================================
 
 pkgs <- c("data.table", "lfe", "geosphere", "Rcpp", "RcppArmadillo")
 invisible(sapply(pkgs, require, character.only = TRUE))
 sourceCpp("code/cpp-functions.cpp")
 
-ConleySEs <- function(reg,
-    unit, time, lat, lon,
-    kernel = "bartlett", dist_fn = "Haversine",
-    dist_cutoff = 500, lag_cutoff = 5,
-    lat_scale = 111, verbose = FALSE, cores = 1, balanced_pnl = FALSE) {
+ConleySEs <- function(model,
+                     unit, time, lat, lon,
+                     kernel = "bartlett", dist_fn = "Haversine",
+                     dist_cutoff = 500, lag_cutoff = 5,
+                     lat_scale = 111, verbose = FALSE, cores = 1,
+                     balanced_pnl = FALSE) {
 
-    Fac2Num <- function(x) {as.numeric(as.character(x))}
-    source("code/iterate-obs-function.R", local = TRUE)
-    if(cores > 1) {invisible(library(parallel))}
+  Fac2Num <- function(x) { as.numeric(as.character(x)) }
 
-    if(class(reg) == "felm") {
-        Xvars <- rownames(reg$coefficients)
-        dt = data.table(reg$cY, reg$cX,
-            fe1 = Fac2Num(reg$fe[[1]]),
-            fe2 = Fac2Num(reg$fe[[2]]),
-            coord1 = Fac2Num(reg$clustervar[[1]]),
-            coord2 = Fac2Num(reg$clustervar[[2]]))
-        setnames(dt,
-            c("fe1", "fe2", "coord1", "coord2"),
-            c(names(reg$fe), names(reg$clustervar)))
-        dt = dt[, e := as.numeric(reg$residuals)]
+  if(cores > 1) { invisible(library(parallel)) }
 
-    } else {
-        message("Model class not recognized.")
-        break
-    }
+  if(class(model) == "felm") {
+    Xvars <- rownames(model$coefficients)
+    dt = data.table(model$cY, model$cX,
+                    fe1 = Fac2Num(model$fe[[1]]),
+                    fe2 = Fac2Num(model$fe[[2]]),
+                    coord1 = Fac2Num(model$clustervar[[1]]),
+                    coord2 = Fac2Num(model$clustervar[[2]]))
+    setnames(dt,
+             c("fe1", "fe2", "coord1", "coord2"),
+             c(names(model$fe), names(model$clustervar)))
+    dt = dt[, e := as.numeric(model$residuals)]
 
-    n <- nrow(dt)
-    k <- length(Xvars)
+  } else {
+    error("Model class not recognized.")
+  }
 
-    # Renaming variables:
-    orig_names <- c(unit, time, lat, lon)
-    new_names <- c("unit", "time", "lat", "lon")
-    setnames(dt, orig_names, new_names)
+  n <- nrow(dt)
+  k <- length(Xvars)
 
-    # Empty Matrix:
-    XeeX <- matrix(nrow = k, ncol = k, 0)
+  # Renaming variables:
+  orig_names <- c(unit, time, lat, lon)
+  new_names <- c("unit", "time", "lat", "lon")
+  setnames(dt, orig_names, new_names)
 
-    #================================================================ 
-    # Correct for spatial correlation:
-    timeUnique <- unique(dt[, time])
-    Ntime <- length(timeUnique)
-    setkey(dt, time)
+  # Empty Matrix:
+  XeeX <- matrix(nrow = k, ncol = k, 0)
 
-    if(verbose){message("Starting to loop over time periods...")}
-    
-    if(balanced_pnl){
-        sub_dt <- dt[time == timeUnique[1]]
-        lat <- sub_dt[, lat]; lon <- sub_dt[, lon]; rm(sub_dt)
-        
-        if(balanced_pnl & verbose){message("Computing Distance Matrix...")}
-        
-        d <- DistMat(cbind(lat, lon), cutoff = dist_cutoff, kernel, dist_fn)
-        rm(list = c("lat", "lon"))
-    }
-    
-    if(cores == 1) {
-        XeeXhs <- lapply(timeUnique, function(t) iterateObs(sub_index = t,
-            type = "spatial", cutoff = dist_cutoff))
-    } else {
-        XeeXhs <- mclapply(timeUnique, function(t) iterateObs(sub_index = t,
-            type = "spatial", cutoff = dist_cutoff), mc.cores = cores)
-    }
-    
-    if(balanced_pnl){rm(d)}
-	
-    # First Reduce:
-	XeeX <- Reduce("+",  XeeXhs)
+  #================================================================
+  # Correct for spatial correlation:
+  timeUnique <- unique(dt[, time])
+  Ntime <- length(timeUnique)
+  setkey(dt, time)
 
-    # Generate VCE for only cross-sectional spatial correlation:
-    X <- as.matrix(dt[, eval(Xvars), with = FALSE])
-    invXX <- solve(t(X) %*% X) * n
+  if(verbose){ message("Starting to loop over time periods...") }
 
-    V_spatial <- invXX %*% (XeeX / n) %*% invXX / n
+  if(balanced_pnl){
+    sub_dt <- dt[time == timeUnique[1]]
+    lat <- sub_dt[, lat]; lon <- sub_dt[, lon]; rm(sub_dt)
 
-    V_spatial <- (V_spatial + t(V_spatial)) / 2
+    if(balanced_pnl & verbose){ message("Computing Distance Matrix...") }
 
-    if(verbose) {message("Computed Spatial VCOV.")}
+    d <- DistMat(cbind(lat, lon), cutoff = dist_cutoff, kernel, dist_fn)
+    rm(list = c("lat", "lon"))
+  }
 
-    #================================================================ 
-    # Correct for serial correlation:
-    panelUnique <- unique(dt[, unit])
-    Npanel <- length(panelUnique)
-    setkey(dt, unit)
+  if(cores == 1) {
+    XeeXhs <- lapply(timeUnique, function(t) {
+      iterateObs(dt, Xvars,
+                 sub_index = t,
+                 type = "spatial",
+                 cutoff = dist_cutoff,
+                 balanced_pnl = balanced_pnl,
+                 kernel = kernel,
+                 dist_fn = dist_fn,
+                 verbose = verbose)
+    })
+  } else {
+    XeeXhs <- mclapply(timeUnique,function(t) {
+      iterateObs(dt, Xvars,
+                 sub_index = t,
+                 type = "spatial",
+                 cutoff = dist_cutoff,
+                 balanced_pnl = balanced_pnl,
+                 kernel = kernel,
+                 dist_fn = dist_fn,
+                 verbose = verbose)},
+      mc.cores = cores)
+  }
 
-    if(verbose){message("Starting to loop over units...")}
+  if(balanced_pnl){ rm(d) }
 
-    if(cores == 1) {
-        XeeXhs <- lapply(panelUnique, function(t) iterateObs(sub_index = t,
-            type = "serial", cutoff = lag_cutoff))
-    } else {
-        XeeXhs <- mclapply(panelUnique,function(t) iterateObs(sub_index = t,
-            type = "serial", cutoff = lag_cutoff), mc.cores = cores)
-    }
+  # First Reduce:
+  XeeX <- Reduce("+",  XeeXhs)
 
-	XeeX_serial <- Reduce("+",  XeeXhs)
+  # Generate VCE for only cross-sectional spatial correlation:
+  X <- as.matrix(dt[, eval(Xvars), with = FALSE])
+  invXX <- solve(t(X) %*% X) * n
 
-	XeeX <- XeeX + XeeX_serial
+  V_spatial <- invXX %*% (XeeX / n) %*% invXX / n
 
-    V_spatial_HAC <- invXX %*% (XeeX / n) %*% invXX / n
-    V_spatial_HAC <- (V_spatial_HAC + t(V_spatial_HAC)) / 2
+  V_spatial <- (V_spatial + t(V_spatial)) / 2
 
-    return_list <- list(
-        "OLS" = reg$vcv,
-        "Spatial" = V_spatial,
-        "Spatial_HAC" = V_spatial_HAC)
-    return(return_list)
+  if(verbose) {message("Computed Spatial VCOV.")}
+
+  #================================================================
+  # Correct for serial correlation:
+  panelUnique <- unique(dt[, unit])
+  Npanel <- length(panelUnique)
+  setkey(dt, unit)
+
+  if(verbose){ message("Starting to loop over units...") }
+
+  if(cores == 1) {
+    XeeXhs <- lapply(panelUnique, function(t) {
+      iterateObs(dt, Xvars,
+                 sub_index = t,
+                 type = "serial",
+                 cutoff = lag_cutoff,
+                 balanced_pnl = balanced_pnl,
+                 kernel = kernel,
+                 dist_fn = dist_fn,
+                 verbose = verbose)
+    })
+  } else {
+    XeeXhs <- mclapply(panelUnique,function(t) {
+      iterateObs(dt, Xvars,
+                 sub_index = t,
+                 type = "serial",
+                 cutoff = lag_cutoff,
+                 balanced_pnl = balanced_pnl,
+                 kernel = kernel,
+                 dist_fn = dist_fn,
+                 verbose = verbose)},
+      mc.cores = cores)
+  }
+
+  XeeX_serial <- Reduce("+",  XeeXhs)
+
+  XeeX <- XeeX + XeeX_serial
+
+  V_spatial_HAC <- invXX %*% (XeeX / n) %*% invXX / n
+  V_spatial_HAC <- (V_spatial_HAC + t(V_spatial_HAC)) / 2
+
+  return_list <- list(
+    "OLS" = model$vcv,
+    "Spatial" = V_spatial,
+    "Spatial_HAC" = V_spatial_HAC)
+  return(return_list)
 }
-
-
-
-
